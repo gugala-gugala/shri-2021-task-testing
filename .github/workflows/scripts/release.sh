eba58438865cc11e762babe0f522989b79dbf2b0
gugala-gugala <70020478+gugala-gugala@users.noreply.github.com>
2021-10-31 22:39:08 +0300

    Update release.sh

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -3,5 +3,47 @@ echo $(curl "$HOST"'/v2/boards/' \
   --header 'Authorization: OAuth '"$YA_TOKEN" \
   --header 'X-Org-ID: '"$CLIENT_ID" \
 )
-echo YA_TOKEN=$YA_TOKEN
-echo CLIENT_ID=$CLIENT_ID
+
+export CURRENT_TAG=$(git tag | tail -1 | head -n1)
+PREV_TAG=$(git tag | tail -2 | head -n1)
+export CURRENT_TAG_AUTHOR=$(git show "$CURRENT_TAG" --pretty="format:%an" --no-patch -1)
+export CURRENT_TAG_DATE=$(git show "$CURRENT_TAG" --pretty="format:%ar" --no-patch -1)
+CHANGELOG=$(git log "$PREV_TAG" --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n" " ")
+
+export QUEUE_NAME="TMP"
+export UNIQUE_VALUE="test"
+
+export REQUEST_BODY='{
+  "queue": "'$QUEUE_NAME'",
+  "summary": "'"$CURRENT_TAG"'| Author: '"$CURRENT_TAG_AUTHOR"' | released '"$CURRENT_TAG_DATE"'",
+  "description": "'"$CHANGELOG"'",
+  "unique": "'"$UNIQUE_VALUE"'_'"$CURRENT_TAG"'"
+}'
+
+#----------------------------------------
+# создаем тикет в Яндекс-трекере
+#----------------------------------------
+
+bash ./.github/workflows/scripts/add_task_in_tracker.sh
+
+if [ $? != 0 ]; then
+  exit $?
+fi
+
+#----------------------------------------
+# Тесты
+#----------------------------------------
+bash ./.github/workflows/scripts/tests.sh
+
+if [ $? != 0 ]; then
+  exit $?
+fi
+
+#----------------------------------------
+# Артефакт
+#----------------------------------------
+bash ./.github/workflows/scripts/artifact.sh
+
+if [ $? != 0 ]; then
+  exit $?
+fi
