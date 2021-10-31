5c0a0835d3d6e30d5b975153671e683e2a78d8a7
gugala-gugala <70020478+gugala-gugala@users.noreply.github.com>
2021-10-31 22:12:33 +0300

    Update release.sh

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-
+echo $(curl -o /dev/null -s -w "%{http_code}\n" "$HOST"'/v2/boards/' \
+  --header 'Authorization: OAuth '"$YA_TOKEN" \
+  --header 'X-Org-ID: '"$CLIENT_ID" \
+)
 echo YA_TOKEN=$YA_TOKEN
 echo CLIENT_ID=$CLIENT_ID
